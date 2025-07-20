a6c8f585d601495c9a183c2ee9ad3212b5aa91ba
Lil Kev <kjolly544@gmail.com>
2025-07-20 18:05:06 -0400

    Update Envoys.php

--- src/bajan/Envoys/Envoys.php
+++ src/bajan/Envoys/Envoys.php
@@ -1,95 +1,52 @@
 <?php
 
-namespace bajan\envoys;
+declare(strict_types=1);
+
+namespace bajan\Envoys;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\block\Chest;
 use pocketmine\math\Vector3;
-use pocketmine\utils\TextFormat as TF;
-use pocketmine\level\Position;
-use pocketmine\Player;
 use pocketmine\scheduler\ClosureTask;
-use pocketmine\tile\Tile;
-use pocketmine\tile\Chest as TileChest;
-use pocketmine\inventory\ChestInventory;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\block\Block;
+use pocketmine\utils\TextFormat;
 
 class Envoys extends PluginBase implements Listener {
 
-    /** @var array */
-    private $activeCrates = [];
-
-    /** @var RewardManager */
-    private $rewardManager;
+    private EnvoyManager $envoyManager;
+    private int $interval;
 
     public function onEnable(): void {
-        @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
+        $this->saveResource("rewards.yml");
+        $this->saveResource("messages.yml");
+
+        $this->interval = $this->getConfig()->get("envoy-spawn-interval", 300);
+
+        $this->envoyManager = new EnvoyManager($this);
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        $this->rewardManager = new RewardManager($this);
+        $this->scheduleEnvoySpawnTask();
 
-        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function (): void {
-            $this->spawnEnvoys();
-        }), $this->getConfig()->get("envoy-spawn-interval", 60) * 20);
+        $this->getLogger()->info(TextFormat::GREEN . "Envoys plugin enabled!");
     }
 
-    public function spawnEnvoys(): void {
-        $cfg = $this->getConfig();
-
-        $locations = $cfg->get("envoy-spawn-locations", []);
-        $min = $cfg->get("min_envoy", 1);
-        $max = $cfg->get("max_envoy", 3);
-
-        $amount = mt_rand($min, $max);
-
-        $count = 0;
-        foreach ($locations as $name => $loc) {
-            if ($count >= $amount) break;
-
-            $x = mt_rand($loc["x-min"], $loc["x-max"]);
-            $y = mt_rand($loc["y-min"], $loc["y-max"]);
-            $z = mt_rand($loc["z-min"], $loc["z-max"]);
-            $level = $this->getServer()->getDefaultLevel();
-            $pos = new Position($x, $y, $z, $level);
-
-            $level->loadChunk($x >> 4, $z >> 4); // ensure chunk is loaded
-
-            $level->setBlock($pos, Block::get(Block::CHEST));
-            $nbt = Tile::createNBT($pos)->setTag(new StringTag("CustomName", "📦 Tap Me!"));
-            $tile = Tile::createTile(Tile::CHEST, $level, $nbt);
-            if ($tile instanceof TileChest) {
-                $tile->setName("Envoy Crate");
-            }
-
-            $this->activeCrates[] = $pos;
-            $count++;
-        }
-
-        $this->getServer()->broadcastMessage(TF::GOLD . "📦 $count envoy crates have been deployed!");
+    private function scheduleEnvoySpawnTask(): void {
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
+            $this->envoyManager->spawnEnvoys();
+        }), 20 * $this->interval);
     }
 
-    public function onInteract(PlayerInteractEvent $event): void {
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
         $block = $event->getBlock();
         $player = $event->getPlayer();
 
-        foreach ($this->activeCrates as $i => $pos) {
-            if ($block->x === $pos->getX() && $block->y === $pos->getY() && $block->z === $pos->getZ()) {
-                $event->setCancelled(true); // cancel chest opening
-                unset($this->activeCrates[$i]);
-                $block->getLevel()->setBlock($pos, Block::get(Block::AIR));
-                $this->rewardManager->giveReward($player);
-                $player->sendMessage(TF::GREEN . "🎁 You opened an envoy crate!");
-                break;
-            }
-        }
-    }
+        // Only interact with chest blocks
+        if ($block->getTypeId() === VanillaBlocks::CHEST()->getTypeId()) {
+            $pos = $block->getPosition();
 
-    public function getRewardManager(): RewardManager {
-        return $this->rewardManager;
+            $this->envoyManager->handleTap($player, $pos);
+        }
     }
 }
